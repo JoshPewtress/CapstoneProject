00d3ef3567cac224c7580512e018b31f24391788
Josh Pewtress <JoshPewtress@gmail.com>
2023-12-19 18:01:21 -0700

    Add new MongoCallTypeData with interface

---
+++ MapsAndTools/MapsAndTools/MapsAndToolsLibrary/DataAccess/ICallTypeData.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MapsAndToolsLibrary.DataAccess;
+
+public interface ICallTypeData
+{
+	Task CreateCallType(CallTypeModel callType);
+	Task<List<CallTypeModel>> GetAllCallTypes();
+	Task UpdateCallType(CallTypeModel callType);
+}
---
+++ MapsAndTools/MapsAndTools/MapsAndToolsLibrary/DataAccess/MongoCallTypeData.cs
@@ -0,0 +1,38 @@
+﻿namespace MapsAndToolsLibrary.DataAccess;
+
+public class MongoCallTypeData : ICallTypeData
+{
+	private readonly IMongoCollection<CallTypeModel> _callTypes;
+	private readonly IMemoryCache _cache;
+	private const string cacheName = "CallTypesData";
+
+	public MongoCallTypeData(IDbConnection db, IMemoryCache cache)
+	{
+		_cache = cache;
+	}
+
+	public async Task<List<CallTypeModel>> GetAllCallTypes()
+	{
+		var output = _cache.Get<List<CallTypeModel>>(cacheName);
+		if (output is null)
+		{
+			var results = await _callTypes.FindAsync(_ => true);
+			output = results.ToList();
+
+			_cache.Set(cacheName, output);
+		}
+
+		return output;
+	}
+
+	public Task CreateCallType(CallTypeModel callType)
+	{
+		return _callTypes.InsertOneAsync(callType);
+	}
+
+	public Task UpdateCallType(CallTypeModel callType)
+	{
+		var filter = Builders<CallTypeModel>.Filter.Eq("Id", callType.Id);
+		return _callTypes.ReplaceOneAsync(filter, callType, new ReplaceOptions { IsUpsert = true });
+	}
+}
